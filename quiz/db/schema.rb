1e4d7cd91c0e8b72bff2d8e463ef6da8776a912b
chynoweth55 <chynowra@plu.edu>
2015-11-23 23:28:01 -0800

    New Column to the questions table. Also, there is a line in the layouts/application.html.erb file that does not work on windows so I deleted it. But from my understanding it doesn't matter if it is there or not.

--- quiz/db/schema.rb
+++ quiz/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118012255) do
+ActiveRecord::Schema.define(version: 20151124072232) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "question"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151118012255) do
     t.string   "four"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "Quiz_Num"
   end
 
   create_table "users", force: :cascade do |t|
